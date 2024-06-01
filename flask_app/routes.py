8ebff50b4d52c04208e2c04ab6bdb9f2b6f018fd
David1497 <david.junior1214@gmail.com>
2024-06-01 22:06:30 +0100

    added bootstrap to forms

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, request, flash
+from flask import render_template, redirect, url_for, request, flash, session, jsonify
 from flask_login import current_user, login_user, logout_user, login_required
 from urllib.parse import urlsplit
 import sqlalchemy as sa
@@ -27,10 +27,8 @@ def overview():
 
 @app.route('/auth', methods=["GET", "POST"])
 def login():
-    print(f'\n\nGot in the login method\n\n')
 
     if current_user.is_authenticated:
-        print(f'\n\nDEBUG:Current user is authenticated and should be able to move to overview\n\n')
         return redirect(url_for('overview'))
     
     form = LoginForm(request.form)
@@ -38,7 +36,6 @@ def login():
     if form.validate_on_submit():
         user = db.session.scalar(
             sa.select(User).where(User.username == form.username.data))
-        print(f"Got the user {user}")
         if user is None or not user.check_password(form.password.data):
             flash(f'Invalid username {form.username.data}')
             return redirect(url_for('login'))
@@ -82,3 +79,19 @@ def signup():
         return redirect(url_for('login'))
 
     return render_template('signup.html', form=form)
+
+
+
+@app.route('/get_user_info', methods=["GET"])
+def get_user_info():
+    if 'user_id' in session:
+        user_id = session['user_id']
+        user = User.query.get(user_id)
+        user_info = {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email
+            # Add other user info as needed
+        }
+        return jsonify(user_info)
+    return jsonify({'error': 'User not logged in'}), 401
